d834a7d760ddd9c0314bae74f6aca4cea6683768
Dominique Horard <53867975+dominiquehorard@users.noreply.github.com>
2021-06-11 10:35:22 -0400

    Update main.js
    moved the open request above the onload because we want the request to be ready when the page loads

--- main.js
+++ main.js
@@ -6,13 +6,14 @@ let button = document.getElementById('get_degrees');
 
 /*event handler for click event on the button*/
 /*contains and IIFE that has the function for parsing/displaying JSON data and get request*/
-button.addEventListener("click", function (e){
+button.addEventListener("click", function (e){        
+    /*the request that states where to get the information from and if it's asynchronous*/
+    /*not sure why this open request is in here, DOUBLE CHECK WHERE THIS SHOULD BE....MAYBE OUTSIDE OF THIS FUNC*/
+    xhr.open('GET', 'degrees_earned.json',true);
     /*function that defines what to do with the JSON data*/
     xhr.onload = function () {
         if (xhr.status === 200) {        
-        /*the request that states where to get the information from and if it's asynchronous*/
-        /*not sure why this open request is in here, DOUBLE CHECK WHERE THIS SHOULD BE....MAYBE OUTSIDE OF THIS FUNC*/
-        xhr.open('GET', 'degrees_earned.json',true);
+
             /*storing the response text in the JSON file in a variable for later use*/
             let responseObject = JSON.parse(xhr.responseText);
             /*log the response object to the console*/
