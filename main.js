e7d19149b2621c0f910721185e9b5c54889a9304
Dominique Horard <53867975+dominiquehorard@users.noreply.github.com>
2021-06-11 17:37:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,22 +7,6 @@ let button = document.getElementById('get_degrees');
 /*storing the div where the json data will be displayed in a variable for use later*/
 let displayDiv = document.getElementById('degrees_div');
 
-/*function that is called in the anon function for the event listener*/
-function displayData(data) {
-    /*variable that will store the strings created out of json data*/
-    let dataContent = '';
-    /*for loop that goes through the json data*/
-    /*for each element in the file, it creates an html element*/
-    for (i = 0; i < data.length; i++) {
-        /*creates html element for each object in the json file*/
-        dataContent += '<h3>' + data[i].school + '</h3>';
-        dataContent += '<p class="type">' + data[i].type + '</p>';
-        dataContent += '<p class="major">' + data[i].major + '</p>';
-        dataContent += '<p class="year">' + data[i].year + '</p>';
-    }
-    displayDiv.insertAdjacentHTML('afterbegin',dataContent);
-}
-
 /*event handler for click event on the button*/
 /*contains and anon func that has the function for parsing/displaying JSON data and get request*/
 button.addEventListener("click", function (e){
@@ -42,3 +26,18 @@ button.addEventListener("click", function (e){
     xhr.send();
 });
 
+/*function that is called in the anon function for the event listener*/
+function displayData(data) {
+    /*variable that will store the strings created out of json data*/
+    let dataContent = '';
+    /*for loop that goes through the json data*/
+    /*for each element in the file, it creates an html element*/
+    for (i = 0; i < data.length; i++) {
+        /*creates html element for each object in the json file*/
+        dataContent += '<h3>' + data[i].school + '</h3>';
+        dataContent += '<p class="type">' + data[i].type + '</p>';
+        dataContent += '<p class="major">' + data[i].major + '</p>';
+        dataContent += '<p class="year">' + data[i].year + '</p>';
+    }
+    displayDiv.insertAdjacentHTML('afterbegin',dataContent);
+}
