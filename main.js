8efa4acf83365c1856aaccd67a898c5e016d7fbc
Dominique Horard <53867975+dominiquehorard@users.noreply.github.com>
2021-06-11 10:23:36 -0400

    Update main.js
    moved the request up because there was an issue with sending the request when it was at the end of the function

--- main.js
+++ main.js
@@ -9,16 +9,15 @@ let button = document.getElementById('get_degrees');
 button.addEventListener("click", function (e){
     /*function that defines what to do with the JSON data*/
     xhr.onload = function () {
-        if (xhr.status === 200) {
+        if (xhr.status === 200) {        
+        /*the request that states where to get the information from and if it's asynchronous*/
+        /*not sure why this open request is in here, DOUBLE CHECK WHERE THIS SHOULD BE....MAYBE OUTSIDE OF THIS FUNC*/
+        xhr.open('GET', 'degrees_earned.json',true);
             /*storing the response text in the JSON file in a variable for later use*/
             let responseObject = JSON.parse(xhr.responseText);
             /*log the response object to the console*/
             console.log(resposnseObject);
         }
-
-        /*the request that states where to get the information from and if it's asynchronous*/
-        /*not sure why this open request is in here, DOUBLE CHECK WHERE THIS SHOULD BE....MAYBE OUTSIDE OF THIS FUNC*/
-        xhr.open('GET', 'degrees_earned.json',true);
     };
 
     /*send the request to the server*/
