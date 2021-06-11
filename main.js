79a803a5bb3cd6303c83d131df46b9f8807bef1f
Dominique Horard <53867975+dominiquehorard@users.noreply.github.com>
2021-06-10 21:00:03 -0400

    Update main.js
    changed url for the request to the JSON file name since it's in the same directory

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ xhr.onload = function () {
 }
 
 /*the request that states where to get the information from and if it's asynchronous*/
-xhr.open('GET', 'https://api.jsonbin.io/b/60c22a344d024768b8f5d619',true);
+xhr.open('GET', 'degrees_earned.json',true);
 
 /*send the request to the server*/
 xhr.send(null);
