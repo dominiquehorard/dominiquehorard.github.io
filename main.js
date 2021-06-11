40e1be3c2254c93141cf9fcb2956d7b04a4ff4f2
Dominique Horard <53867975+dominiquehorard@users.noreply.github.com>
2021-06-11 12:36:07 -0400

    Update main.js
    added the displayData function
    called the function in the anon function for the event handler

--- main.js
+++ main.js
@@ -4,23 +4,29 @@ let xhr = new XMLHttpRequest();
 /*storing the button element in a variable for later use with event handler*/
 let button = document.getElementById('get_degrees');
 
+/*storing the div where the json data will be displayed in a variable for use later*/
+let displayDiv = document.getElementById('degrees_div');
+
 /*event handler for click event on the button*/
-/*contains and IIFE that has the function for parsing/displaying JSON data and get request*/
-button.addEventListener("click", function (e){        
+/*contains and anon func that has the function for parsing/displaying JSON data and get request*/
+button.addEventListener("click", function (e){
     /*the request that states where to get the information from and if it's asynchronous*/
-    /*not sure why this open request is in here, DOUBLE CHECK WHERE THIS SHOULD BE....MAYBE OUTSIDE OF THIS FUNC*/
     xhr.open('GET', 'degrees_earned.json',true);
     /*function that defines what to do with the JSON data*/
     xhr.onload = function () {
-        if (xhr.status === 200) {        
-
+        if (xhr.status === 200) {
             /*storing the response text in the JSON file in a variable for later use*/
             let responseObject = JSON.parse(xhr.responseText);
             /*log the response object to the console*/
             console.log(responseObject);
         }
+        displayData(responseObject);
     };
-
     /*send the request to the server*/
     xhr.send();
 });
+
+/*function that is called in the anon function for the event listener*/
+function displayData(jsonObject) {
+    displayDiv.insertAdjacentHTML('beforebegin', 'test')
+}
