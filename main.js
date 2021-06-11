7b34d514c46f549d2315b636f57ed5e9a06438fe
Dominique Horard <53867975+dominiquehorard@users.noreply.github.com>
2021-06-10 21:28:28 -0400

    Update main.js
    /*storing the button element in a variable for later use with event handler*/
    /*event handler for click event on the button*/
    /*contains and IIFE that has the function for parsing/displaying JSON data and get request*/
    /*not sure why this open request is in here, DOUBLE CHECK WHERE THIS SHOULD BE....MAYBE OUTSIDE OF THIS FUNC*/

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 /*creating a request object*/
 let xhr = new XMLHttpRequest();
 
-/*function that defines what to do with the JSON data*/
-xhr.onload = function () {
-    if (xhr.status === 200) {
-        /*storing the response text in the JSON file in a variable for later use*/
-        responseObject = JSON.parse(xhr.responseText);
-    }
-}
+/*storing the button element in a variable for later use with event handler*/
+let button = document.getElementById('get_degrees');
 
-/*the request that states where to get the information from and if it's asynchronous*/
-xhr.open('GET', 'degrees_earned.json',true);
+/*event handler for click event on the button*/
+/*contains and IIFE that has the function for parsing/displaying JSON data and get request*/
+button.addEventListener("click", function (e){
+    /*function that defines what to do with the JSON data*/
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            /*storing the response text in the JSON file in a variable for later use*/
+            let responseObject = JSON.parse(xhr.responseText);
+        }
 
-/*send the request to the server*/
-xhr.send(null);
+        /*the request that states where to get the information from and if it's asynchronous*/
+        /*not sure why this open request is in here, DOUBLE CHECK WHERE THIS SHOULD BE....MAYBE OUTSIDE OF THIS FUNC*/
+        xhr.open('GET', 'degrees_earned.json',true);
+    };
+
+    /*send the request to the server*/
+    xhr.send(null);
+});
