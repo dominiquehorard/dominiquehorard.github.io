8622b1c393de9fc110e3a3abb7f79444d424b2f6
Dominique Horard <53867975+dominiquehorard@users.noreply.github.com>
2021-06-18 14:34:03 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            arch:'img/gallery_imgs/img373.jpg',
+            catBox:'img/gallery_imgs/img374.jpg',
+            treeLimb:'img/gallery_imgs/img375.jpg',
+            creek:'img/gallery_imgs/img376.jpg',
+            trees:"img/gallery_imgs/img379.jpg",
+            maintenanceBuilding:"img/gallery_imgs/img380.jpg",
+            snowyCar:"img/gallery_imgs/img383.jpg",
+            snowyBackyard:"img/gallery_imgs/img384.jpg",
+            snowyTree:"img/gallery_imgs/img385.jpg",
+            snowTreeRoof:"img/gallery_imgs/img386.jpg",
+            sunnyRoof:"img/gallery_imgs/img389.jpg",
+            cozyDog:"img/gallery_imgs/img390.jpg",
+            sunnyTree:"img/gallery_imgs/img391.jpg",
+            sittingDog:"img/gallery_imgs/img392-2.jpg",
+            blurCat:"img/gallery_imgs/img393.jpg",
+            darkPic:"img/gallery_imgs/img394.jpg"
+        }
+    }
+})
